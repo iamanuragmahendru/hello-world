8995e35b765ead9f7d92a9b88094fd4b19d431c4
Anurag Mahendru <iamanuragmahendru@gmail.com>
2019-01-05 20:59:42 +0530

    Modified file add.cpp

--- add.cpp
+++ add.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 
     int a = 10, b = 5;
-    cout << "The sum of " << a << " and " << b << " = " << (a+b) << endl;
+    int c = a + b;
+    cout << "The sum of " << a << " and " << b << " = " << c << endl;
 
 }
